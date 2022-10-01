5ec8a411e41a6ce48c4379fd0ddc5005103fc99c
NearTruth <55720636+NearTruth@users.noreply.github.com>
2022-09-30 21:12:32 -0400

    Completed all tasks in Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,8 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +63,8 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,8 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: "+i);
+        }
     }
 
     /**
@@ -141,7 +144,13 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
+        
+        String[] split = to_split.split(" ");
+        for(String s: split) {
+            ret.append(s.charAt(0));
+        }
+        
+        
         // Fill in the rest of the body here
 
         return ret.toString();
@@ -163,14 +172,16 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
-
+        for(int i = 1; i<arr.length; i+=2) {
+            current_sum += arr[i];
+        }
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+            
         return current_sum;
     }
 
